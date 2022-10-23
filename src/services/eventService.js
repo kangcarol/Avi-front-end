a25f52989a0a02b33cdc5be7964de6dd536bef53
trmorningstar <trmorningstar@gmail.com>
2022-10-23 19:59:01 -0400

    finish eventEdit, eventDelete, eventNew

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -14,10 +14,62 @@ const index = async () => {
   }
 }
 
+const create = async (eventData) => {
+  // blogData will have a shape of:
+//   {
+//     "title": "string",
+//     "category": "string",
+//     "text": "string"
+//   }
+  try {
+    // POST http://localhost:3001/events
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        "Authorization": `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(eventData)
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+const update = async (eventData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${eventData._id}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(eventData)
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deleteEvent = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+      }
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export {
   index,
   // show,
-  // create,
-  // update,
-  // deleteEvent
+  create,
+  update,
+  deleteEvent
 }
