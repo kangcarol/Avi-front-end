6ddbd2fed0826a5472a1b68ba01fb8b7beabc3d7
kangcarol <otherhats@gmail.com>
2022-10-24 17:39:53 -0400

    in App.css add padding for visibility

--- src/pages/BirdDetails/BirdDetails.jsx
+++ src/pages/BirdDetails/BirdDetails.jsx
@@ -30,8 +30,7 @@ const BirdDetails = (props) => {
     <main className={styles.container}>
       <article>
         <header>
-          {/* <h3>{bird.name.toUpperCase()}</h3> */}
-          <h1>{bird.name.toUpperCase()}</h1>
+          <h3>{bird.name.toUpperCase()}</h3>
           <span>
             {/* <AuthorInfo content={bird} /> */}
             {/* {bird.author._id === props.user.profile && */}
