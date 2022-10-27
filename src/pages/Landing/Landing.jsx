10f843bce0f7371c95c000e072c54a77a231e266
kangcarol <otherhats@gmail.com>
2022-10-27 12:03:25 -0400

    change out className style names to pertain to the component, so as to no override

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
+    <main className={styles.landing}>
     </main>
   )
 }
