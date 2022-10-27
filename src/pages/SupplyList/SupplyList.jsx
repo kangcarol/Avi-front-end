86991faa680f75318eb3bc69f00d03a6dbbc299d
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-10-27 00:34:11 -0400

    style supply list page

--- src/pages/SupplyList/SupplyList.jsx
+++ src/pages/SupplyList/SupplyList.jsx
@@ -3,15 +3,18 @@ import SupplyListCard from '../../components/SupplyCard/SupplyCard'
 import { useNavigate } from 'react-router-dom'
 
 const SupplyList = (props) => {
-  console.log('SUPPLYLIST', props)
   const navigate = useNavigate()
+
   return (
-    <main className={styles.container}>
-      <button onClick={() => {navigate('/supplylists/new')}}>Make A SupplyList</button>
-      {props.supplyLists.map((supplylist) => (
-        <SupplyListCard key={supplylist._id} supplylist={supplylist} />
-      ))}
-    </main>
+    <>
+    <h1>Supply Lists</h1>
+      <main className={styles.container}>
+        <button class="btn btn-secondary" onClick={() => {navigate('/supplylists/new')}}>Make A SupplyList</button>
+        {props.supplyLists.map((supplylist) => (
+          <SupplyListCard key={supplylist._id} supplylist={supplylist} />
+          ))}
+      </main>
+    </>
   )
 }
 
